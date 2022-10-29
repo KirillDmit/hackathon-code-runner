61007b7ab2ff65d89b645fe9b02b8456b8b7f71d
KirillDmit <kirilldmitriev20015@gmail.com>
2022-10-29 15:15:13 +0500

    Update HackathonCodeRunnerApplication.java

--- src/main/java/com/example/hackathon_code_runner/HackathonCodeRunnerApplication.java
+++ src/main/java/com/example/hackathon_code_runner/HackathonCodeRunnerApplication.java
@@ -2,8 +2,9 @@ package com.example.hackathon_code_runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HackathonCodeRunnerApplication {
 
     public static void main(String[] args) {
