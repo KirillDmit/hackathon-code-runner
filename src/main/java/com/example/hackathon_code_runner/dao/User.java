78e575aa1458026dcd064445590ed4bc6a630579
KirillDmit <kirilldmitriev20015@gmail.com>
2022-10-29 20:05:55 +0500

    Create User.java

---
+++ src/main/java/com/example/hackathon_code_runner/dao/User.java
@@ -0,0 +1,92 @@
+package com.example.hackathon_code_runner.dao;
+
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.stereotype.Component;
+
+@Component
+@Table(name = "user")
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String socialNetwork;
+    private String phone;
+    private String location;
+    private String specialization;
+
+    public User(int id, String name, String surname, String patronymic, String socialNetwork, String phone, String location, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.socialNetwork = socialNetwork;
+        this.phone = phone;
+        this.location = location;
+        this.specialization = specialization;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSocialNetwork() {
+        return socialNetwork;
+    }
+
+    public void setSocialNetwork(String socialNetwork) {
+        this.socialNetwork = socialNetwork;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
