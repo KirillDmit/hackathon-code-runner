5ce3c0fd9a8132608a54567a3272a8ba91f6d39c
KirillDmit <kirilldmitriev20015@gmail.com>
2022-10-29 14:35:07 +0500

    Initial commit

---
+++ src/main/java/com/example/hackathon_code_runner/HackathonCodeRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_code_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonCodeRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonCodeRunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackathon_code_runner/HackathonCodeRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_code_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonCodeRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
